401f4ffc7b8a26e3fdf236270ad5d125bbae7c66
Jacob Deguara <jacob.deguara.20@um.edu.mt>
2022-10-25 20:25:08 +0200

    Player Commit + Clothing on Array Commit

--- Map.h
+++ Map.h
@@ -2,6 +2,8 @@
 #include <ncurses.h>
 #include <bits/stdc++.h>
 #include <vector>
+#include <iostream>
+#include "Player.h"
 
 #ifndef _MAP_H_
 #define _MAP_H_
@@ -15,19 +17,19 @@ private:
     //Menu Selection Needed Variables
     WINDOW * menuwin;
     WINDOW * submenuwin;
+    WINDOW * reswin;
     int highlight, highlight2; //this holds the higlighted menu
     int choice, choice2; // this holds the choice done by the user
-    const char * str; // this is a temporary const char for printing
     struct { // this struct holds each menu  Name followed by its sub menus: Names and size
         string mName;
-        string smNames[5];
+        vector<string> smNames;
         int choiceSize;
     }choices[5];
     void choicesCreation(); //this is used for formulating the strings for Choice
-
-
-
+    
 public:
+
+    Player p;
 
     Map(WINDOW * menuWindow, WINDOW * submenuWindow, WINDOW * resourceWindow);
     //getters
@@ -47,6 +49,7 @@ Map::Map(WINDOW * menuWindow, WINDOW * submenuWindow, WINDOW * resourceWindow){
     //initilize menus and highlights
     menuwin = menuWindow;
     submenuwin = submenuWindow;
+    reswin = resourceWindow;
     highlight = 0;
     highlight2 =-1;
     //innitilizeing the menu system
@@ -61,24 +64,22 @@ Map::Map(WINDOW * menuWindow, WINDOW * submenuWindow, WINDOW * resourceWindow){
 void Map::choicesCreation(){
     choices[0].mName = "> Create Building";
     choices[2].mName = "> Upgrade Building";
-    choices[0].smNames[0] = choices[2].smNames[0] = "> Wood Cutter   ";
-    choices[0].smNames[1] = choices[2].smNames[1] = "> Stone Miner   ";
-    choices[0].smNames[2] = choices[2].smNames[2] = "> Iron Miner    ";
-    choices[0].smNames[3] = choices[2].smNames[3] = "> Butcher       ";
-    choices[0].smNames[4] = choices[2].smNames[4] = "> Battle Trainer";
+    
+    string str[] ={"> Wood Cutter   ","> Stone Miner   ","> Iron Miner    ","> Butcher       ","> Battle Trainer"};
+    choices[0].smNames.assign(str,str+5);
+    choices[2].smNames.assign(str,str+5);
     choices[0].choiceSize = choices[2].choiceSize = 5;
 
     choices[1].mName = "> Create Troops";
     choices[3].mName = "> Upgrade Troops";
-    choices[1].smNames[0] = choices[3].smNames[0] = "> Archer        ";
-    choices[1].smNames[1] = choices[3].smNames[1] = "> Knight        ";
-    choices[1].smNames[2] = choices[3].smNames[2] = "> Defender      ";
+    string str2[] = {"> Archer        ", "> Knight        ", "> Defender      " };
+    choices[1].smNames.assign(str2,str2+3);
+    choices[3].smNames.assign(str2,str2+3);
     choices[1].choiceSize = choices[3].choiceSize = 3;
     
     choices[4].mName = "> Actions";
-    choices[4].smNames[0] = "> Attack         ";
-    choices[4].smNames[1] = "> Skip           ";
-    choices[4].smNames[2] = "> Surrender      ";
+    string str3[] = {"> Attack         ","> Skip           ","> Surrender      "};
+    choices[4].smNames.assign(str3,str3+3);
     choices[4].choiceSize = 3;
 }
 
@@ -126,8 +127,8 @@ int Map::mgetmv(){
 
         case KEY_DOWN:
             highlight2++;
-            if(highlight2 == (choices[highlight].choiceSize)){
-                highlight2 = (choices[highlight].choiceSize)-1;
+            if(highlight2 == choices[highlight].smNames.size()){
+                highlight2 = choices[highlight].smNames.size() - 1;
             }
             break;
 
@@ -146,6 +147,8 @@ int Map::mgetmv(){
 
 
 void Map::display(){
+    const char * str; 
+    mvwprintw(menuwin,0,1,"Menu-(up/down/left)");
     for (int i = 0; i < 5; i++)
     {
         if(i == highlight){ //if highlighted
@@ -156,25 +159,61 @@ void Map::display(){
         wattroff(menuwin,A_REVERSE);    
     }
 
+    mvwprintw(submenuwin,0,1,"Submenu-(up/down/enter)");
     for (int i = 0; i < 5; i++)
     {   
+        if(i >= choices[highlight].smNames.size()){
+            mvwprintw(submenuwin,i+1,1,"                  "); //remove string
+            continue;
+        }
+
         if(choices[highlight].smNames[i]!=""){ //if not empty
             if(i == highlight2){ //if highlighted
                 wattron(submenuwin,A_REVERSE);
             }
-            str = (choices[highlight].smNames[i]).c_str();
+            str = (choices[highlight].smNames[i].data());
             mvwprintw(submenuwin,i+1,1,str); //print string
-        }else{
-            mvwprintw(submenuwin,i+1,1,"                  "); //remove string
         }
         
         wattroff(submenuwin,A_REVERSE);    
+    }
+
+    mvwprintw(reswin,0,1,"Resources");
+    for(int i = 0; i < p.getResSize(); i++)
+    {   
+        string t;
+        int num;
+        switch(i){
+            case 0:
+                str = "Wood  :";
+                num = p.getWood();
+                break;
+            case 1:
+                str = "Stone :";
+                num = p.getStone();
+                break;
+            case 2:
+                str = "Iron  :";
+                num = p.getIron();
+                break;
+            case 3:
+                str = "Food  :";
+                num = p.getFood();
+                break;
+            case 4:
+                break;
+            default:
+            break;
+        }
+        mvwprintw(reswin,i+1,1,"%s%d",str,num);
+
     }
 }
 
 int Map::getHighlight(){
     return highlight;
 }
+
 int Map::getHighlight2(){
     return highlight2;
 }
--- main.cpp
+++ main.cpp
@@ -6,7 +6,84 @@
 #include "Map.h"
 
 using namespace std;
-int main(void){	
+
+WINDOW * rWin;
+WINDOW * mmWin;
+WINDOW * smWin;
+WINDOW * iWin;
+WINDOW * mapWin;
+
+void refreshWindows(){
+    wrefresh(rWin);
+    wrefresh(mmWin);
+    wrefresh(smWin);
+    wrefresh(iWin);
+    wrefresh(mapWin);
+}
+
+void createWindows(int yMax,int xMax){
+    //windows Created    
+        //resourse window
+        rWin = newwin(yMax*5/8,xMax*3/8,0,0); 
+        box(rWin,0,0); //create box
+        refresh();
+        wrefresh(rWin);
+        keypad(rWin,true);
+
+        //main menu Window
+        mmWin = newwin((yMax*3/8)+1,xMax*2/8,yMax*5/8,0); //new window
+        box(mmWin,0,0); //create box
+        refresh();
+        wrefresh(mmWin);
+        keypad(mmWin,true);
+
+        //sub menu Window
+        smWin = newwin((yMax*3/8)+1,xMax*2/8,yMax*5/8,xMax*2/8); //new window
+        box(smWin,0,0); //create box
+        refresh();
+        wrefresh(smWin);
+        keypad(smWin,true);
+        
+        //Input Window
+        iWin = newwin((yMax*3/8)+1,xMax*4/8,yMax*5/8,xMax*4/8); //new window
+        box(iWin,0,0); //create box
+        refresh();
+        wrefresh(iWin);
+        keypad(iWin,true);
+
+        //Map Window
+        mapWin = newwin(yMax*5/8,xMax*5/8,0,xMax*3/8); //new window
+        box(mapWin,0,0); //create box
+        refresh();
+        wrefresh(mapWin);
+        keypad(mapWin,true);
+}
+
+int gameLoop(Map * m){
+    int mNum=-2;
+    //-2 is main menu, -1 is sub menu, 0+ is a selection.
+    do{
+        do{
+            m->display();
+            refreshWindows();
+            mNum = m->getmv();
+        }while(mNum == -2);
+        
+        m->prepSubMenu();
+
+        do{
+            m->display();
+            refreshWindows();
+            mNum = m->mgetmv();
+        }while(mNum == -1);
+    }while(mNum < 0);
+    
+
+    return 0;
+}
+
+int main(void){
+    //stuff i need before running ncurses
 	initscr(); 
     noecho();
     cbreak();
@@ -16,64 +93,12 @@ int main(void){
     int yMax, xMax;
     getmaxyx(stdscr,yMax,xMax);
 
-    //windows Created    
-        //resourse window
-        WINDOW * rWin = newwin(yMax*5/8,xMax*3/8,0,0); 
-        box(rWin,0,0); //create box
-        refresh();
-        wrefresh(rWin);
-        keypad(rWin,true);
-
-        //main menu Window
-        WINDOW * mmWin = newwin((yMax*3/8)+1,xMax*3/8,yMax*5/8,0); //new window
-        box(mmWin,0,0); //create box
-        refresh();
-        wrefresh(mmWin);
-        keypad(mmWin,true);
-
-        //sub menu Window
-        WINDOW * smWin = newwin((yMax*3/8)+1,xMax*3/8,yMax*5/8,xMax*3/8); //new window
-        box(smWin,0,0); //create box
-        refresh();
-        wrefresh(smWin);
-        keypad(smWin,true);
-        
-        //Input Window
-        WINDOW * iWin = newwin((yMax*3/8)+1,xMax*2/8,yMax*5/8,xMax*6/8); //new window
-        box(iWin,0,0); //create box
-        refresh();
-        wrefresh(iWin);
-        keypad(iWin,true);
-
-        //Map Window
-        WINDOW * mapWin = newwin(yMax*5/8,xMax*5/8,0,xMax*3/8); //new window
-        box(mapWin,0,0); //create box
-        refresh();
-        wrefresh(mapWin);
-        keypad(mapWin,true);
-    //Creation
+    //create Windows
+    createWindows(yMax,xMax);
     
     Map * m = new Map(mmWin,smWin,rWin);
-    int mNum=-2;
-
-    //-2 is main menu, -1 is sub menu, 0+ is a selection.
-    do{
-        do{
-            m->display();
-            wrefresh(mmWin);
-            wrefresh(smWin);
-            mNum = m->getmv();
-        }while(mNum == -2);
-        
-        m->prepSubMenu();
-
-        do{
-            m->display();
-            wrefresh(mmWin);
-            wrefresh(smWin);
-            mNum = m->mgetmv();
-        }while(mNum == -1);
-    }while(mNum < 0);
+    
+    gameLoop(m);
 
     cout << m->getHighlight() << "   " << m->getHighlight2();
     endwin();
